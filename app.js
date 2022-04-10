292983bbb4625447a44ef826b5e238f5e82486f6
suzi <suzivoskanian@gmail.com>
2022-04-11 00:22:11 +0400

    changes

--- app.js
+++ app.js
@@ -1,48 +1,18 @@
-const currentDay = new Date().getDay();
-
-switch(new Date().getDay()){
-    case 0:
-        console.log("new Date().getDay()===Sunday");
-        break;
-    case 1:
-        console.log('new Date().getDay()===Monday');
-        break;
-    case 2:
-        console.log("new Date().getDay()===Tuesday");
-        break;
-    case 3:
-        console.log("new Date().getDay()===Wednesday");
-        break;
-    case 4:
-        console.log("new Date().getDay()===Thursday");
-        break;
-    case 5:
-        console.log("new Date().getDay()===Friday");
-        break;
-    case 6:
-        console.log("new Date().getDay()===Saturday");
-    default:
-        console.log('new Date().getDay()===other day');
-        break;
-}
-
-
-
-
+// 2
 let myArr = new Array(100);
     for (let i = 0; i < myArr.length; i++) {
     console.log(myArr[i]);
     }
 
 
-
+// 3
 let i=0;
 while(i<51){
     console.log(i);
     i++};
 
 
-
+// 4
 let j=0;
 do{
     console.log(j);
@@ -50,8 +20,38 @@ do{
 }while(j<151);
 
 
+// 5
 let myArr2=[i]
 while(myArr2<1001){
     console.log(myArr2)
     myArr2++
 } 
+
+// 1
+const currentDay = new Date().getDay();
+
+switch(new Date().getDay()){
+    case 0:
+    console.log("new Date().getDay()===Sunday");
+    break;
+    case 1:
+    console.log('new Date().getDay()===Monday');
+    break;
+    case 2:
+    console.log("new Date().getDay()===Tuesday");
+    break;
+    case 3:
+    console.log("new Date().getDay()===Wednesday");
+    break;
+    case 4:
+    console.log("new Date().getDay()===Thursday");
+    break;
+    case 5:
+    console.log("new Date().getDay()===Friday");
+    break;
+    case 6:
+    console.log("new Date().getDay()===Saturday");
+    default:
+    console.log('new Date().getDay()===other day');
+    break;
+}
